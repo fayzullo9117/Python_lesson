8048dd275787466ec95a3646e523d80a26015a32
Maestro9117 <136426403+fayzullo9117@users.noreply.github.com>
2023-09-23 16:33:09 +0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,65 @@
+# x = "salom"
+# y = 17
+# z = 1.2
+
+# a = str(1)
+# b = int(1)
+# c = float(1)
+
+# print(x)
+# print(y)
+# print(z)
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# print("*****************************")
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# x = "Assalom alekum"
+
+# y = input()
+
+# print(f'{x} {y}')
+
+# def fun(y):
+#     print("Salom " + y)
+
+# x = "dunyo"
+# fun(x)
+
+# x = 5
+# y = 3
+
+# print(f"{x + y} -- {x-y} -- {x*y} -- {x/y} -- {x%y} -- {x**y} -- {x//y}")
+
+# print(x&3)
+
+
+# a = "python"
+# b = 39
+# c = ('salom','hayr','ko`rishguncha')
+# d = 1j
+
+# print(f"{type(a)} {type(b)} {type(c)} {type(d)}")
+
+# def fun():
+#     global x 
+#     x = "fantastic"
+
+# fun()
+
+# print('Python is ' + x)
+
+# def myfunc():
+#   global x
+#   x = "fantastic"
+
+# x = "awesome"
+# myfunc()
+
+# print("Python is " + x)
