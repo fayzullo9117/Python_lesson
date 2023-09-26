44bc44637c14f7275e65bac8fb3f64e7baf7b559
Maestro9117 <136426403+fayzullo9117@users.noreply.github.com>
2023-09-26 22:33:07 +0500

    Nesting
    Nestingga qo`shimcha kiritildi

--- 10_Nesting.py
+++ 10_Nesting.py
@@ -48,41 +48,87 @@
 
 # **********AMALIYOT**********
 
-shaxs0  = {
-    'ism':"Abu abdulloh Muhammad ibn ismoil",
-    'tyili':818,
-    'shahar':"buxoro",
-    'yoshi': 60,
-    'asarlari':['al-jome as-sahih','al-adab al-mufrat','at-tarix as-sag`ir']
-}
+# shaxs0  = {
+#     'ism':"Abu abdulloh Muhammad ibn ismoil",
+#     'tyili':818,
+#     'shahar':"buxoro",
+#     'yoshi': 60,
+#     'asarlari':['al-jome as-sahih','al-adab al-mufrat','at-tarix as-sag`ir']
+# }
 
-shaxs1 = {
-    "ism":'Abdulla Qodiriy',
-    'tyili':1894,
-    'shahar':'toshkent',
-    'yoshi':44,
-    'asarlari':['otkan kunlar','mehrobdan chayon','obit ketmon']
-}
-shaxs2 = {
-    'ism': 'erkin vohidov',
-    'tyili':1936,
-    'shahar':'farg`ona',
-    'yoshi': 80,
-    'asarlari':['tong nafasi','qoshiqlarim siri','o`zbegim','qiziquvchan matmusa']
-}
+# shaxs1 = {
+#     "ism":'Abdulla Qodiriy',
+#     'tyili':1894,
+#     'shahar':'toshkent',
+#     'yoshi':44,
+#     'asarlari':['otkan kunlar','mehrobdan chayon','obit ketmon']
+# }
+# shaxs2 = {
+#     'ism': 'erkin vohidov',
+#     'tyili':1936,
+#     'shahar':'farg`ona',
+#     'yoshi': 80,
+#     'asarlari':['tong nafasi','qoshiqlarim siri','o`zbegim','qiziquvchan matmusa']
+# }
 
-shaxs3 = {
-    'ism':'Alisher navoiy',
-    'tyili':1441,
-    'shahar':'xirot',
-    'yoshi':60,
-    'asarlari':['Xamsa','lison ut-tayr','mahbub ul-qulub']
-}
+# shaxs3 = {
+#     'ism':'Alisher navoiy',
+#     'tyili':1441,
+#     'shahar':'xirot',
+#     'yoshi':60,
+#     'asarlari':['Xamsa','lison ut-tayr','mahbub ul-qulub']
+# }
 
+# greet_humans = [shaxs0,shaxs1,shaxs2,shaxs3]
 
-greet_humans = [shaxs0,shaxs1,shaxs2,shaxs3]
+# for x in greet_humans:
+#     print(f"{x['ism'].title()} {x['tyili']}-yilda {x['shahar'].title()}da tavallud topgan.{x['yoshi']} yil umr ko`rgan")
+#     for asar in x['asarlari']:
+#         print(asar.title())
 
-for x in greet_humans:
-    print(f"{x['ism'].title()} {x['tyili']}-yilda {x['shahar'].title()}da tavallud topgan.{x['yoshi']} yil umr ko`rgan")
-    for asar in x['asarlari']:
-        print(asar.title())
+# 2
+
+# humans = {
+#     "alisher":['terminator','Rambo','titanic'],
+#     'vali':['tenet','inception','interstellar'],
+#     'husan':['mahallada duv-duv gap','john wick']
+# }
+
+# for h in humans.keys():
+#     print(f"\n{h.title()} ning sevimli kinolari: ")
+#     for x in humans[h]:
+#         print(x.title())
+
+# 3
+
+# countries = {
+#     'country_1': {
+#         'uzbekistan':'tashkent',
+#         'huhudi': 448978,
+#         'aholisi':3300000,
+#         'pul birligi': 'so`m'
+#     },
+#     'country_2': {
+#         'rossia':'moskva',
+#         'huhudi': 17098246,
+#         'aholisi':144000000,
+#         'pul birligi': 'rubl'
+#     },
+#     'country_3': {
+#         'aqsh':'washington',
+#         'huhudi': 9631418,
+#         'aholisi':327000000,
+#         'pul birligi': 'dollar'
+#     },
+#     'country_4': {
+#         'malayziya':'kuala-lumpur',
+#         'huhudi': 329750,
+#         'aholisi':25000000,
+#         'pul birligi': 'rinngit'
+#     }
+
+# }
+
+# for country in countries.values():
+#     for x in country.keys():
+#         print(f"{x.title()} - {country[x]}")
---
+++ 11_while.py
@@ -0,0 +1,20 @@
+# num = 1
+# while num <= 5:
+#     print(num,end=' ')
+#     num += 1
+
+# print("Kiritilgan sonning kvadratini qaytaruvchi dastur.")
+# savol = "Istalgan son kiriting "
+# savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+# qiymat = ''
+# while qiymat != 'exit':
+#     qiymat = input(savol)
+#     if qiymat != 'exit':
+#         print(float(qiymat)**2)
+
+print("Yoqtirgan kitoblaringiz nomini kiriting \nva yakunlasj uchun stop so`zini yozing")
+qiymat = ''
+num = 1
+while qiymat.lower() != 'stop':
+    qiymat = input()
+
