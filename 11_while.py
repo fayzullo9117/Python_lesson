e8e19db387640043415dc0a401e99468f05dd510
Maestro9117 <136426403+fayzullo9117@users.noreply.github.com>
2023-10-07 14:01:40 +0500

    Update 11_while

--- 11_while.py
+++ 11_while.py
@@ -12,9 +12,24 @@
 #     if qiymat != 'exit':
 #         print(float(qiymat)**2)
 
-print("Yoqtirgan kitoblaringiz nomini kiriting \nva yakunlasj uchun stop so`zini yozing")
-qiymat = ''
-num = 1
-while qiymat.lower() != 'stop':
-    qiymat = input()
+# print("Yoqtirgan kitoblaringiz nomini kiriting \nva yakunlasj uchun stop so`zini yozing")
+# qiymat = ''
+# num = 1
+# while qiymat.lower() != 'stop':
+#     qiymat = input()
 
+print("Chipta narxini bilish uchun yoshingizni kiriting: ")
+value = ''
+while value != 'exit' or value != 'quit':
+    value = input()
+    if int(value) > 0:
+        if int(value) < 7:
+            print("7 yoshgacha bolalar uchun chipta narxi 2000 so`m")
+        elif int(value) >= 7 and int(value) < 18:
+            print("7 yoshdan 18 yosh oraligidagi bolalar uchun chipta narxi 3000 so`m")
+        elif int(value) >= 18 and int(value) < 65:
+            print("18 yoshdan 65 yosh oralig`idagilar uchun chipta narxi 10000 so`m")
+        else:
+            print("65 yoshdan yuqori yoshdagilar uchun chipta narxi bepul")
+    else:
+        print("Xatolik kuzatildi iltimos boshqa qiymat kiriting")
